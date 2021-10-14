9ffddc8f0656e5b25a42ea5001cb5b3133160d1c
DatVTP <vanthanhphuongdat1309@gmail.com>
2021-10-14 16:51:08 +0700

    done

--- hometest-dropdown/src/Dropdown.jsx
+++ hometest-dropdown/src/Dropdown.jsx
@@ -1,14 +1,41 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function Dropdown ({selected, setSelected}) {
     const [isActive, setIsActive] = useState(false)
-    const options = ['Pizza', 'Hamburger', 'Sandwich', 'Ice cream', 'Soda']
+    const inputRef = useRef()
+    const defaultOptions = ['Pizza', 'Hamburger', 'Sandwich', 'Ice cream', 'Soda']
+    const [options, setOptions] = useState(defaultOptions)
     return (
         <div className="dropdown">
+            <div style={{textAlign: "center", marginBottom: "10%"}}>
+                <h3> This is a dropdown for choosing food.</h3>
+                <h4> Input to search your food.</h4>
+            </div>
+
             <div className="dropdown-button" onClick={e => {
                 setIsActive(!isActive)
             }}>
-                {selected ? selected : 'Choose one food'}
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Choose one food"
+                    ref={inputRef}
+                    onChange={e => {
+                        if (inputRef.current.value) {
+                            setOptions(
+                                defaultOptions.filter((option) => {
+                                    if(option.toUpperCase().includes(inputRef.current.value.toUpperCase())) {
+                                        return option
+                                    }
+                                    return null
+                                })
+                            )
+                        } else {
+                            setOptions(defaultOptions)
+                            setSelected('')
+                        }
+                    }}
+                />
                 <span><i className={isActive ? "up-arrow" : "down-arrow"}></i></span>
             </div>
             {isActive && (
@@ -18,6 +45,7 @@ function Dropdown ({selected, setSelected}) {
                         className="dropdown-item" 
                         onClick={(e) => {
                             setSelected(option);
+                            inputRef.current.value = option;
                             setIsActive(false);
                         }}>
                             {option}
